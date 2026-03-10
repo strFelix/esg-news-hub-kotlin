ce2b556a25e28d0e21a954240e58169d6ae4c321
marianaeslan <mariana.eslan@gmail.com>
2026-03-10 19:42:28 -0300

    feat: changes logo image

--- app/src/main/java/br/com/strfelix/esg_news_hub_kotlin/screens/InitialScreen.kt
+++ app/src/main/java/br/com/strfelix/esg_news_hub_kotlin/screens/InitialScreen.kt
@@ -1,6 +1,7 @@
 package br.com.strfelix.esg_news_hub_kotlin.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,8 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Newspaper
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -33,8 +34,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import br.com.strfelix.esg_news_hub_kotlin.R
+import br.com.strfelix.esg_news_hub_kotlin.screens.components.BackgroundPosition
+import br.com.strfelix.esg_news_hub_kotlin.screens.components.BackgroundVector
 import br.com.strfelix.esg_news_hub_kotlin.ui.theme.EsgBackgroundGradient
-
 
 
 @Composable
@@ -45,6 +48,8 @@ fun InitialScreen(navController: NavController) {
             .background(EsgBackgroundGradient)
             .padding(24.dp)
     ) {
+        BackgroundVector(position = BackgroundPosition.Top)
+        BackgroundVector(position = BackgroundPosition.Bottom)
         HeaderSection(
             modifier = Modifier
                 .align(Alignment.TopCenter)
@@ -96,11 +101,10 @@ fun CenterSection(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        Icon(
-            imageVector = Icons.Default.Newspaper,
-            contentDescription = null,
+        Icon (
+            painter = painterResource(id = R.drawable.logo_esg),
+            contentDescription = "Logo ESG News",
             tint = Color.Black,
-            modifier = Modifier.size(72.dp)
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
